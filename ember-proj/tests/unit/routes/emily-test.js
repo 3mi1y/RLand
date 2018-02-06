cb5cb1f45766f4cf0fac116c52c14307d898d17b
3mi1y <emily@portal.org>
2018-02-06 13:07:30 -0700

    Added login route

---
+++ ember-proj/tests/unit/routes/emily-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:emily', 'Unit | Route | emily', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
