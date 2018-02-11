50447d9e4f8fd97c06e9954eb6f9ec4391ee8739
jeb <jeb@jebrosen.com>
2018-02-10 21:08:45 -0700

    convenience script to (hopefully) clear out the whole database

---
+++ server/reset_db.py
@@ -0,0 +1,11 @@
+from storage.riak import RiakDb
+
+def clear_bucket(bucket):
+    keys = bucket.get_keys()
+    for k in keys:
+        obj = bucket.get(k)
+        obj.delete()
+
+db = RiakDb()
+clear_bucket(db.user_bucket)
+clear_bucket(db.poly_bucket)
