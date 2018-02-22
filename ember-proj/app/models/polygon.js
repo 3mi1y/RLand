a618fa476c325862783153d4aef82ef3642ae4a5
3mi1y <emily@portal.org>
2018-02-21 20:47:56 -0700

    Added polygon and user models with relationships

--- ember-proj/app/models/polygon.js
+++ ember-proj/app/models/polygon.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
    ID: DS.attr('string'),
    Name: DS.attr('string'),
-   Location: DS.attr('string')
+   Location: DS.attr('string'),
+   user: DS.belongsTo('user')
 });
