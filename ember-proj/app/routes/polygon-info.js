dbe82b38aef694348f25fad4738b0184b73cdeac
3mi1y <emily@portal.org>
2018-03-04 21:01:48 -0700

    Fixed view one unique polygon so you can see details

--- ember-proj/app/routes/polygon-info.js
+++ ember-proj/app/routes/polygon-info.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
    model(params) {
-     //console.log(this.store.recordIsLoaded('polygon', params.poalygon_id))
-     return this.store.findRecord('polygon', 1)
+     return this.store.findRecord('polygon', params.polygon_id)
    }
 });
