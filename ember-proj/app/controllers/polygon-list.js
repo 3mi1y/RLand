1b7e50c63530fafe8e95d11a02feaaef5a3fcf13
3mi1y <emily@portal.org>
2018-03-04 21:28:24 -0700

    Now have the functionality to delete a record

--- ember-proj/app/controllers/polygon-list.js
+++ ember-proj/app/controllers/polygon-list.js
@@ -3,11 +3,16 @@ import Controller from '@ember/controller';
 export default Controller.extend({
    actions: {
       getPolygons() {
-      this.get('store').findAll('polygon').then(function(polygons) {
-          polygons.forEach(function(p) {
-            console.log(p.id);
-          });
-      })
-    }
+         this.get('store').findAll('polygon').then(function(polygons) {
+             polygons.forEach(function(p) {
+             console.log(p.id);
+         });
+       })
+     },
+     deletePolygon(polygon) {
+       this.store.findRecord('polygon', polygon.get('id')).then((post) => {
+          post.destroyRecord();
+       })
+     }
    }
 });
