cf636e01f401b2b58fc34ea3f2a3df40627f102a
3mi1y <emily@portal.org>
2018-03-06 13:43:44 -0700

    Edit polygon functionality complete

--- ember-proj/app/controllers/edit-polygon.js
+++ ember-proj/app/controllers/edit-polygon.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+   actions: {
+      submitChangedPolygon (id) {
+         let location = this.get('location')
+         let name = this.get('name')
+         this.store.findRecord('polygon', id).then((changedPolygon) => {
+            changedPolygon.set('name', name)
+            changedPolygon.set('location', location)
+            changedPolygon.save()
+         }).then(
+            this.transitionToRoute('polygon-list')
+         )
+      }
+   }
 });
