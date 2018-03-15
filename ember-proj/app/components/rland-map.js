d4b17b8e38c53a3d514608d5124ce22079cce466
Jesse D <jesse.davidson@umconnect.umt.edu>
2018-03-15 01:35:15 -0600

    added some clarification comments for modification

--- ember-proj/app/components/rland-map.js
+++ ember-proj/app/components/rland-map.js
@@ -54,23 +54,3 @@ export default Ember.Component.extend({
     // }
   }
 });
-// import Component from '@ember/component';
-// import { inject as service } from '@ember/service';
-//
-// export default Component.extend({
-//   maps: service(),
-//   classNames: "rland-map",
-//
-//   init()
-//   {
-//     this._super(...arguments);
-//     // this.get('maps').addObserver('selected', this, 'polygon_selected');
-//   },
-//
-//   didInsertElement() {
-//     this._super(...arguments);
-//     let location = this.get('location');
-//     let mapElement = this.get('maps').getMapElement(location);
-//     this.$('.map-container').append(mapElement);
-//   }
-// });
