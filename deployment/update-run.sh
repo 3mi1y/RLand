9dbbac6f0107916e1e0212a26e02c804cf223e33
jeb <jeb@jebrosen.com>
2018-03-26 15:53:57 -0600

    add deployment instructions and sample script files

---
+++ deployment/update-run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rland_dir=$HOME/rland
+
+source "$HOME/.nvm/nvm.sh"
+source "$HOME"/venv/bin/activate
+
+cd "$rland_dir"
+git pull
+
+cd "$rland_dir"/ember-proj
+ember build
+
+cd "$rland_dir"/server
+python serve2.py --port=8080
