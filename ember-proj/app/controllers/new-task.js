f9bd214950fa2cddc25d0b6204eb9480ce669f67
3mi1y <emily@portal.org>
2018-04-01 11:35:18 -0600

    Can add a task, not integrated with backend api yet

--- ember-proj/app/controllers/new-task.js
+++ ember-proj/app/controllers/new-task.js
@@ -2,8 +2,14 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
    actions: {
-      createNewTask() {
-         console.log(params.polygon_id)
+      addTask() {
+         let task = this.get('store').createRecord('task', {
+            name: this.get('name'),
+            description: this.get('description'),
+            dueDate: this.get('dueDate'),
+            priority: this.get('priority')
+         })
+         task.save()
       }
    }
 });
--- ember-proj/app/models/task.js
+++ ember-proj/app/models/task.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+   name: DS.attr('string'),
+   description: DS.attr('string'),
+   priority: DS.attr('number'),
+   dueDate: DS.attr('string'),
+   polygon: DS.belongsTo('polygon')
 });
