21c0bface1403f6f55a2292d88d83cfaa1defe26
3mi1y <emily@portal.org>
2018-03-31 22:43:48 -0600

    Updated add task form and links to form

--- ember-proj/app/controllers/polygon-list.js
+++ ember-proj/app/controllers/polygon-list.js
@@ -3,10 +3,10 @@ import Controller from '@ember/controller';
 export default Controller.extend({
    actions: {
       getPolygons() {
-         this.get('store').peekAll('polygon').then(function(polygons) {
-            // polygons.forEach(function(p) {
-             //console.log(p.id);
-         //});
+         this.get('store').findAll('polygon').then(function(polygons) {
+            polygons.forEach(function(p) {
+            console.log(p.id);
+         });
        })
      },
      deletePolygon(polygon) {
