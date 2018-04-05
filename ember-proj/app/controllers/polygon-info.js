9907451891dc984d532104b54544931b25d3fbb3
3mi1y <emily@portal.org>
2018-04-04 20:39:11 -0600

    Major UI overhaul for polygon info page, still in progress

--- ember-proj/app/controllers/polygon-info.js
+++ ember-proj/app/controllers/polygon-info.js
@@ -2,6 +2,11 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
    actions: {
+      deleteTask(task) {
+         this.store.findRecord('task', task.get('id')).then((post) => {
+          post.destroyRecord();
+       })
+      },
       deleteNote(note) {
          this.store.findRecord('note', note.get('id')).then((post) => {
             post.destroyRecord()
--- ember-proj/app/routes/polygon-info.js
+++ ember-proj/app/routes/polygon-info.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
    model(params) {
-     return this.store.findRecord('polygon', params.polygon_id)
+    // return this.store.findRecord('polygon', params.polygon_id)
+      return RSVP.hash({
+        polygon: this.store.findRecord('polygon', params.polygon_id),
+        tasks: this.store.query('task', { polyId: params.polygon_id }),
+        notes: this.store.query('note', { polyId: params.polygon_id})
+      })
    }
 });
