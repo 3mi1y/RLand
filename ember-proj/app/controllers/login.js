038a70f1add87087f15db6d43501df03bd044df9
jeb <jeb@jebrosen.com>
2018-04-08 13:52:29 -0600

    reformat login.js controller for code clarity and fix order of login operations

--- ember-proj/app/controllers/login.js
+++ ember-proj/app/controllers/login.js
@@ -17,22 +17,24 @@ export default Controller.extend({
       $.post("api/login", {
         email: this.get("email"),
         password: this.get("password")
-      }).then(function () {
-          this.set("isProcessing", false);
-          
-          this.get('store').findRecord('user', this.get('email')).then(() => {
-              this.transitionToRoute('map')},
-            (response) => {
-              $('#errors').text(response.errors[0].title);-
-              $('#errors').show();
-            })
-        }.bind(this),
+      }).then(() => {
+        // login reported success
+        this.set("isProcessing", false);
 
-        function () {
-          this.set("isProcessing", false);
-          this.set("loginFailed", true);
-        }.bind(this));
-        this.get('authentication').login();
+        this.get('store').findRecord('user', this.get('email')).then(() => {
+          // got user
+          this.get('authentication').login();
+          this.transitionToRoute('map')
+        }, (response) => {
+          // failed to get user
+          $('#errors').text(response.errors[0].title);
+          $('#errors').show();
+        });
+      }, () => {
+        // login reported failure
+        this.set("isProcessing", false);
+        this.set("loginFailed", true);
+      });
     },
 
     register() {
