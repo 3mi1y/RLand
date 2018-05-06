7484d2badef424588b7afbc079418d5d55f51e83
jeb <jeb@jebrosen.com>
2018-05-06 11:34:15 -0600

    register: disallow register attempt while an attempt is already in progress

--- ember-proj/app/controllers/register.js
+++ ember-proj/app/controllers/register.js
@@ -2,8 +2,15 @@ import Controller from '@ember/controller';
 import $ from 'jquery';
 
 export default Controller.extend({
+  isProcessing: false,
+
   actions: {
     register() {
+      if (this.isProcessing) {
+        return;
+      }
+      this.set('isProcessing', true);
+
       const street = this.get("street");
       const city = this.get("city");
       const state = this.get("state");
@@ -26,11 +33,13 @@ export default Controller.extend({
           this.get('store').unloadAll();
           $('#register').hide();
           $('#confirm').show();
+          this.set('isProcessing', false);
         },
         (response) => {
           $('#errors').text(response.errors[0].title);
           $('#errors').show();
-      })
+          this.set('isProcessing', false);
+      });
     }
   }
 });
